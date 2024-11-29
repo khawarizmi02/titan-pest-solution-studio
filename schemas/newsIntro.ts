7f8e1245d456dfe64a3c75ceb278b5169c095d1f
khawarizmi <khawarizmijefri02@gmail.com>
2024-11-29 16:12:17 +0800

    Refactor newsIntro.ts schema fields

--- schemas/newsIntro.ts
+++ schemas/newsIntro.ts
@@ -6,26 +6,11 @@ export default defineType({
   type: 'document',
 
   fields: [
-    // defineField({
-    //   name: 'title',
-    //   title: 'Title',
-    //   type: 'string',
-    // }),
-
     defineField({
       name: 'introduction',
       title: 'Introduction',
       type: 'array',
       of: [{type: 'block'}] as any,
     }),
-
-    // defineField({
-    // 	name: 'image',
-    // 	title: 'Page Image',
-    // 	type: 'image',
-    // 	options: {
-    // 		hotspot: true,
-    // 	},
-    // }),
   ],
 })
